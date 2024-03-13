03d9a0fce1b3e669e936ee9f120e3f345cb95a38
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-03-13 15:31:04 -0500

    FOOTER: poisitioning

--- src/Components/ParentsComponents/3.0-Footer.jsx
+++ src/Components/ParentsComponents/3.0-Footer.jsx
@@ -4,12 +4,28 @@ import React from "react";
 //sass
 import '../../Sass/SassComponents/SassParentsComponents/3.0-Footer.scss';
 
-export const Footer = () => {
+export const Footer = () => {  
 
 
     return(
        <footer className="footerContainer">
-            <section className="footerWrapper one" > f1 </section>
+            <section className="footerWrapper one" >
+
+                <div className="wrapperInfo" >
+                    <h1>tittle footer</h1>
+
+                    <div className="wrapperTitle" >
+                        <h3>tittle 2</h3>
+                        <p>lorem basic</p>    
+
+                    </div>
+ 
+                    
+                </div> 
+
+                
+            </section>
+
             <section  className="footerWrapper two"  > f2 </section>
             <section  className="footerWrapper three"  > f3 </section>
        </footer>
