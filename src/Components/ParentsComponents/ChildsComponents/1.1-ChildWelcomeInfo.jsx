bc1d46d359b937c8040c41e590a31939ca3c2a31
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-03-13 07:57:05 -0500

    CARDS:  centering cards

--- src/Components/ParentsComponents/ChildsComponents/1.1-ChildWelcomeInfo.jsx
+++ src/Components/ParentsComponents/ChildsComponents/1.1-ChildWelcomeInfo.jsx
@@ -7,13 +7,14 @@ export const ChildWelcomeInfo = () => {
 
 
     return(
+        
         <section className="welcomeInfoWrapper" >
 
             <h1 className="tittle" >welcome</h1>
 
             <p>
 
-                ¡Hola! Soy Daniel, un apasionado desarrollador web frontend con experiencia en la 
+                ¡Hola! Soy Daniel, un apasionado desarrollador web fullStack con experiencia en la 
                 creación de aplicaciones web responsivas, dinámicas y funcionales. Mi enfoque principal
                 es escribir código limpio y eficiente utilizando tecnologías modernas.
                   
