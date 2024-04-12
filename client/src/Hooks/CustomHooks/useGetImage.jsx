b08131bcfbc63f46b373d4260a37b29af8b3402b
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-04-12 12:10:12 -0500

    cv:changed

--- client/src/Hooks/CustomHooks/useGetImage.jsx
+++ client/src/Hooks/CustomHooks/useGetImage.jsx
@@ -9,7 +9,7 @@ import cvImage from '../../public/assets/SocialMedia/cv.png';
 import whatsappImage  from '../../public/assets/SocialMedia/whatsapp.png';
 
 //cv
-import curriculumVitae from '../../public/assets/SocialMedia/CV_ENGL_0.2.1.pdf';
+import curriculumVitae from '../../public/assets/SocialMedia/CV_ENGL_LETTER_4.0.pdf';
  
 
 //carousel
